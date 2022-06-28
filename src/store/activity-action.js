57ba27ad00fd3db82eb643b5d69b6856affea79d
akash-1712 <akashbansalab01@gmail.com>
2022-06-29 00:21:08 +0530

    deployedFirst

---
+++ src/store/activity-action.js
@@ -0,0 +1,55 @@
+import { activityActions } from "./activity-slice";
+
+export const FetchActivityData = (page = 1, isLoggedIn = false) => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      // console.log(page);
+      let response;
+      if (!isLoggedIn) {
+        response = await fetch(
+          "https://react-timer-app-backend.herokuapp.com/activity/get?page=" +
+            page,
+          {
+            method: "GET",
+            headers: {
+              "content-type": "application/json",
+            },
+          }
+        );
+      } else {
+        const token = localStorage.getItem("token");
+        response = await fetch(
+          "https://react-timer-app-backend.herokuapp.com/activity/authGet?page=" +
+            page,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+      }
+
+      if (!response.ok) {
+        // console.log(response);
+        return;
+      }
+      const data = await response.json();
+      return data;
+    };
+    try {
+      const activityData = await fetchData();
+      dispatch(
+        activityActions.replaceActivity({
+          activityData: activityData.data,
+          totalActivity: activityData.totalActivity,
+          itemsPerPage: activityData.itemsPerPage,
+        })
+      );
+    } catch (err) {
+      // console.log(err);
+      throw err;
+    }
+  };
+};
---
+++ src/store/user-action.js
@@ -0,0 +1,39 @@
+import { UserActions } from "./user-slice.js";
+
+export const fetchUser = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const token = localStorage.getItem("token");
+
+      const response = await fetch(
+        "https://react-timer-app-backend.herokuapp.com/user",
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      if (!response.ok) {
+        // console.log(response);
+      }
+      const data = response.json();
+      return data;
+    };
+
+    try {
+      const userData = await fetchData();
+      const image =
+        "https://react-timer-app-backend.herokuapp.com/" + userData.imageUrl;
+      dispatch(
+        UserActions.replaceUser({
+          name: userData.name,
+          image: image,
+        })
+      );
+    } catch (err) {
+      // console.log(err);
+      throw err;
+    }
+  };
+};
